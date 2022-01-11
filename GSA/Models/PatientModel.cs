b95b7035151ab185b39d73a92af74f5640045551
Abdessamad-Jadid <ajadid@miu.edu>
2022-01-11 12:32:43 -0600

    Add PatientModel

---
+++ GSA/Models/PatientModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GSA.Models
+{
+    public class PatientModel
+    {
+        [Key]
+        public long patientId { get; set; }
+
+        public string patientNumber { get; set; }
+
+        public bool isAnOutPatient { set; get; }
+
+        public string fullNames { set; get; }
+
+        public string emailAddress { set; get; }
+
+        public string contactPhoneNumber {set;get;}
+
+        [DataType(DataType.Date)]
+        [Column(TypeName = "Date")]
+        public DateTime dateOfBirth { get; set; }
+    }
+}
